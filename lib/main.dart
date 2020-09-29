3bfa37eb8b244c0388a888cbdb86cdd5b3b57cc6
AhmedAdly20 <ahmedadly132@gmail.com>
2020-09-29 18:30:47 +0200

    First Web App

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:firstwebapp/constants.dart';
+import 'package:firstwebapp/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Web',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,37 @@
+import 'package:firstwebapp/components/app_bar.dart';
+import 'package:firstwebapp/components/body.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    // This size provide us total height and width  of our screen
+    return Scaffold(
+      body: Container(
+        height: size.height,
+        // it will take full width
+        width: size.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/bg.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            CustomAppBar(),
+            Spacer(),
+            // It will cover 1/3 of free spaces
+            Body(),
+            Spacer(
+              flex: 2,
+            ),
+            // it will cover 2/3 of free spaces
+          ],
+        ),
+      ),
+    );
+  }
+}
